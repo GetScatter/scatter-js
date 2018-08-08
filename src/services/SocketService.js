cac620843756ca79497342936b9b92bdc9123afa
Shai James <skyandbeyond@gmail.com>
2018-08-08 14:53:47 +0300

    errors fix

--- src/services/SocketService.js
+++ src/services/SocketService.js
@@ -60,7 +60,7 @@ export default class SocketService {
                 socket.on('api', result => {
                     const openRequest = openRequests.find(x => x.id === result.id);
                     if(!openRequest) return;
-                    if(result.result.hasOwnProperty('isError')) openRequest.reject(result.result);
+                    if(typeof result.result === 'object' && result.result.hasOwnProperty('isError')) openRequest.reject(result.result);
                     else openRequest.resolve(result.result);
                 });
 
