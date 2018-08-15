4f2f9709d5cf2188e5effbc5e22bfd29af77507f
Shai James <skyandbeyond@gmail.com>
2018-08-16 00:05:06 +0300

    authentication

--- src/services/SocketService.js
+++ src/services/SocketService.js
@@ -8,6 +8,7 @@ const host = 'http://127.0.0.1:50005';
 
 let socket = null;
 let connected = false;
+let paired = false;
 
 let plugin;
 let openRequests = [];
@@ -24,17 +25,45 @@ const reconnectOnAbnormalDisconnection = async () => {
 	}, 1000);
 };
 
+const random = () => {
+    const array = new Uint8Array(24);
+    getRandomValues(array);
+    return array.join('');
+};
 
+const getOrigin = () => {
+    let origin;
+    if(typeof location !== 'undefined')
+        if(location.hasOwnProperty('hostname') && location.hostname.length && location.hostname !== 'localhost')
+            origin = location.hostname;
+        else origin = plugin;
+    else origin = plugin;
+    return origin;
+}
+
+
+StorageService.removeAppKey();
+
+let appkey = StorageService.getAppKey();
+if(!appkey) appkey = 'appkey:'+random();
+
+let pairingPromise = null;
+const pair = () => {
+    return new Promise((resolve, reject) => {
+        pairingPromise = {resolve, reject};
+        socket.emit('pair', {data:{ appkey, origin:getOrigin() }, plugin});
+    })
+}
 
 export default class SocketService {
 
     static init(_plugin, timeout = 60000){
         plugin = _plugin;
-
         this.timeout = timeout;
     }
 
     static async link(){
+
         return Promise.race([
             new Promise((resolve, reject) => setTimeout(async () => {
                 if(connected) return;
@@ -53,7 +82,30 @@ export default class SocketService {
                 socket.on('connected', async () => {
                     clearTimeout(reconnectionTimeout);
                     connected = true;
+
+                    await pair();
                     resolve(true);
+                });
+
+                socket.on('paired', async result => {
+                    paired = result;
+
+                    if(paired) {
+                        const savedKey = StorageService.getAppKey();
+                        const hashed = appkey.indexOf('appkey:') > -1 ? ecc.sha256(appkey) : appkey;
+
+                        if (!savedKey || savedKey !== hashed) {
+                            StorageService.setAppKey(hashed);
+                            appkey = StorageService.getAppKey();
+                        }
+                    }
+
+                    pairingPromise.resolve(result);
+                });
+
+                socket.on('rekey', async () => {
+                    appkey = 'appkey:'+random();
+                    socket.emit('rekeyed', {data:{ appkey, origin:getOrigin() }, plugin});
                 });
 
                 socket.on('event', event => {
@@ -102,52 +154,24 @@ export default class SocketService {
 
     static async sendApiRequest(request){
         return new Promise(async (resolve, reject) => {
+            if(request.type === 'identityFromPermissions' && !paired) return resolve(false);
 
-            StorageService.removeAppKey();
-            StorageService.removeNonce();
-
-            const random = () => {
-                const array = new Uint8Array(24);
-                getRandomValues(array);
-                return array.join('');
-            };
+            if(!paired) {
+                await pair();
+                if(!paired) return reject({code:'not_paired', message:'The user did not allow this app to connect to their Scatter'});
+            }
 
             // Request ID used for resolving promises
             request.id = random();
 
-
-            // App key used to backwards authenticate this browser
-            let appkey = StorageService.getAppKey();
-            if(!appkey){
-                appkey = random();
-                StorageService.setAppKey(ecc.sha256(appkey));
-            }
+            // Set Application Key
             request.appkey = appkey;
-
-            // Nonce used to authenticate this request
-            request.nonce = StorageService.getNonce() || 0;
-
-            // Next nonce used to authenticate the next request
-            const nextNonce = random();
-            request.nextNonce = ecc.sha256(nextNonce);
-            StorageService.setNonce(nextNonce);
-
-
-
 
             console.log(request);
 
+            if(request.hasOwnProperty('payload') && !request.payload.hasOwnProperty('origin'))
+                request.payload.origin = getOrigin();
 
-            if(request.hasOwnProperty('payload') && !request.payload.hasOwnProperty('origin')) {
-                let origin;
-                if(typeof location !== 'undefined')
-                    if(location.hasOwnProperty('hostname') && location.hostname.length && location.hostname !== 'localhost')
-                        origin = location.hostname;
-                    else origin = plugin;
-                else origin = plugin;
-
-                request.payload.origin = origin;
-            }
 
             openRequests.push(Object.assign(request, {resolve, reject}));
             socket.emit('api', {data:request, plugin});
--- src/services/StorageService.js
+++ src/services/StorageService.js
@@ -2,18 +2,6 @@ export default class StorageService {
 
     constructor(){}
 
-    static setNonce(nonce){
-        window.localStorage.setItem('nonce', nonce);
-    };
-
-    static getNonce() {
-        return window.localStorage.getItem('nonce');
-    }
-
-    static removeNonce() {
-        return window.localStorage.removeItem('nonce');
-    }
-
     static setAppKey(nonce){
         window.localStorage.setItem('appkey', nonce);
     };
