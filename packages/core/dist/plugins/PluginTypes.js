19fa7dfc45437d325ddf3e62aa90af5ace7cc095
nsjames <skyandbeyond@gmail.com>
2019-01-14 02:14:07 +0200

    pre-merge

--- packages/core/dist/plugins/PluginTypes.js
+++ packages/core/dist/plugins/PluginTypes.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.BLOCKCHAIN_SUPPORT=void 0;var BLOCKCHAIN_SUPPORT="blockchain_support";exports.BLOCKCHAIN_SUPPORT="blockchain_support";
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.WALLET_SUPPORT=exports.BLOCKCHAIN_SUPPORT=void 0;var BLOCKCHAIN_SUPPORT="blockchain_support";exports.BLOCKCHAIN_SUPPORT="blockchain_support";var WALLET_SUPPORT="wallet_support";exports.WALLET_SUPPORT="wallet_support";
