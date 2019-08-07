28a2b49077bd074074105b190b774b49df1a596b
nsjames <skyandbeyond@gmail.com>
2019-08-08 00:18:02 +0300

    relays to default enabled

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -25,13 +25,9 @@ class Index {
 		this.identity = null;
 		this.network = null;
 
+		PluginRepository.loadPlugin(new RelaySocket(this, holderFns));
 		PluginRepository.loadPlugin(new LocalSocket(this, holderFns));
 		PluginRepository.loadPlugin(new Injection(this, holderFns));
-	}
-
-	useRelay(){
-		console.log('Using relay')
-		PluginRepository.loadPlugin(new RelaySocket(this, holderFns));
 	}
 
 	loadPlugin(plugin){
