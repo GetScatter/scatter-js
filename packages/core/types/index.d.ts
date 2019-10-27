d9378a6fc260631bd9050c9b025dab43376e5a6c
Matei Radu <opensource@mateiradu.dev>
2019-10-27 19:28:03 +0100

    Add main exports

--- packages/core/types/index.d.ts
+++ packages/core/types/index.d.ts
@@ -1,6 +1,58 @@
-export { Blockchains } from "./models/Blockchains";
-export { Network } from "./models/Network";
-export { Token } from "./models/Token"; 
+import { Blockchains } from "./models/Blockchains";
+import { Network } from "./models/Network";
+import { Token } from "./models/Token"; 
 
-export { Plugin } from "./plugins/Plugin";
-export { PluginTypes } from "./plugins/PluginTypes";
+import { Plugin } from "./plugins/Plugin";
+import { PluginTypes } from "./plugins/PluginTypes";
+
+export const EVENTS: {
+    Disconnected: "dced";
+	LoggedOut: "logout";
+};
+
+interface ConnectProps {
+    network?: Network;
+    linkTimeout?: number;
+    allowHttp?: boolean;
+}
+
+class Scatter {
+    identity: any;
+    network: Network;
+    
+    loadPlugin(plugin: Plugin): void;
+    connect(pluginName: string, options?: ConnectProps): Promise<boolean>;
+}
+
+class Holder {
+    scatter: Scatter;
+
+    Plugin = Plugin;
+    PluginTypes: typeof PluginTypes;
+    Blockchains: typeof Blockchains;
+    Network = Network;
+    Token = Token;
+    SocketService = any;
+    EVENTS: typeof EVENTS;
+    WalletInterface = any;
+    WALLET_METHODS: any;
+
+    constructor(scatter: Scatter);
+
+    plugins(...plugins: Plugin[]): void;
+
+    connect(...params: Parameters<Index["connect"]>): ReturnType<Index["connect"]>;
+
+    catchAll(...params: any[]): void
+}
+
+export {
+    Plugin,
+    PluginTypes,
+    Blockchains,
+    Network,
+    EVENTS,
+};
+
+const holder: Holder;
+export default holder;
